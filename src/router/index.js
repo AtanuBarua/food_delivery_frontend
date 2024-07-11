5de377851466ecac74ffc35c3fa02985b97ef2fa
Atanu <niloy.barua74@gmail.com>
2024-07-12 00:33:07 +0600

    owner register page designed

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue';
 import AdminHome from '../views/admin/Home.vue';
+import OwnerRegister from '../views/owner/Register.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -14,6 +15,11 @@ const router = createRouter({
       path: '/admin',
       name: 'adminHome',
       component: AdminHome
+    },
+    {
+      path: '/owner/register',
+      name: 'ownerRegister',
+      component: OwnerRegister
     }
   ]
 })
---
+++ src/views/owner/Register.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="h-screen bg-[url('../assets/cook.jpg')] bg-no-repeat bg-contain flex justify-center items-center">
+        <div class="mx-auto w-[25rem] h-[28rem] bg-white shadow-xl rounded-md p-7">
+            <div class="flex flex-col">
+                <h3 class="text-xl font-bold mb-5">Ready to grow your business?</h3>
+
+                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">Business owner first name
+                    <span class="text-red-600">*</span></label>
+                <input type="text" id="first_name"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">Business owner last name
+                    <span class="text-red-600">*</span></label>
+                <input type="text" id="first_name"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">Business email <span
+                        class="text-red-600">*</span></label>
+                <input type="text" id="first_name"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                <label for="first_name" class="block mb-2 text-sm font-medium text-gray-900">Phone number <span
+                        class="text-red-600">*</span></label>
+                <input type="text" id="first_name"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" />
+                <button class="bg-fuchsia-600 mt-5 rounded-md p-3 text-white">
+                    Get started
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
