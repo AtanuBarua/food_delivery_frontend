7e36733a5b219c93a5601b1b1e147721886b3c53
Atanu <niloy.barua74@gmail.com>
2024-07-28 21:22:35 +0600

    owner routes guard navigation

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue';
-import AdminHome from '../views/admin/Home.vue';
-import OwnerRegister from '../views/owner/Register.vue'
-import OwnerHome from '../views/owner/Home.vue';
-import OwnerLogin from '../views/owner/Login.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AdminHome from "../views/admin/Home.vue";
+import OwnerRegister from "../views/owner/Register.vue";
+import OwnerHome from "../views/owner/Home.vue";
+import OwnerLogin from "../views/owner/Login.vue";
+import { useOwnerStore } from "@/stores/owner";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -19,21 +20,42 @@ const router = createRouter({
       component: AdminHome
     },
     {
-      path: '/owner/register',
-      name: 'ownerRegister',
-      component: OwnerRegister
+      path: "/owner/register",
+      name: "ownerRegister",
+      component: OwnerRegister,
+      meta: {
+        middleware: "ownerGuest",
+      },
     },
     {
-      path: '/owner/login',
-      name: 'ownerLogin',
-      component: OwnerLogin
+      path: "/owner/login",
+      name: "ownerLogin",
+      component: OwnerLogin,
+      meta: {
+        middleware: "ownerGuest",
+      },
     },
     {
-      path: '/owner',
-      name: 'ownerHome',
-      component: OwnerHome
+      path: "/owner",
+      name: "ownerHome",
+      component: OwnerHome,
+      meta: {
+        middleware: "ownerAuth",
+      },
     },
-  ]
-})
+  ],
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  const store = new useOwnerStore();
+  const isAuthenticated = store.isAuthenticated;
+  if (to.meta.middleware == "ownerAuth" && !isAuthenticated) {
+    next({ name: "ownerLogin" });
+  } else if (to.meta.middleware == "ownerGuest" && isAuthenticated) {
+    next({ name: "ownerHome" });
+  } else {
+    next();
+  }
+});
+
+export default router;
